233627b96bfb54f6eef27bcc15535e04d4eccaf0
ahn430 <156993886+ahn430@users.noreply.github.com>
2024-01-31 00:14:49 +0900

    도저히 모르겠다
    힐이 2번인데 i값을 힐 하는걸로 설정해서 i값을 5를 넣으면 체력 5를 회복 그거를 2번해야하잖아 근데 이 i<값을 뭘 넣어야할지 그리고 다른데에서 i값을 어캐넣어어야할지 모르겠다;;

--- Calculator/src/DoubleHeal.java
+++ Calculator/src/DoubleHeal.java
@@ -5,10 +5,11 @@ public class DoubleHeal {
         int resentHP = health;
 
         // i 변수를 이용해서 2번 회복하시오.
-        for() {
-            resentHP += ;
-            boolean fullHP = h.isFullHP();
-            if () {
+        for(int i = 0; i < 3; ++i ) {
+            resentHP +=maxHP ;
+            boolean fullHP = h.isFullHP(maxHP,health,healamount);
+                return;
+            if (fullHP) {
                 System.out.println("최대체력을 넘김");
                 break;
             } else {
