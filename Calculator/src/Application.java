d4792fe9142fd7ec98dbae879b940f17cd9d1a92
U-Jae <77143213+Bungduck@users.noreply.github.com>
2024-02-01 16:52:41 +0900

    Scanner 기초

--- Calculator/src/Application.java
+++ Calculator/src/Application.java
@@ -1,6 +1,8 @@
 import Dictionary.MonsterDictionary;
 import Dictionary.SearchData;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         //~~~~~~~실행하는 것들~~~~~~~~~~~
@@ -38,9 +40,20 @@ public class Application {
 //            System.out.println("일치하는 것을 찾지 못했습니다.");
 //        }
 
+        // 스캐너
+        Scanner sc = new Scanner(System.in);
+        System.out.print("현재 체력을 입력하시오 : ");
+        int health = sc.nextInt();
+        System.out.print("피해 5번 정도 입력하시오 : ");
+        int[] dmgarr = new int[5];
+        for(int i = 0; i < 5; ++i) dmgarr[i] = sc.nextInt();
+
         SumDamage SD = new SumDamage();
-        int dmgarr[] = {1, 2, 3, 4, 5};
-        SD.keepHit(10, dmgarr);
+        SD.keepHit(health, dmgarr);
+
+//        SumDamage SD = new SumDamage();
+//        int dmgarr[] = {1, 2, 3, 4, 5};
+//        SD.keepHit(10, dmgarr);
         
 
 
--- Calculator/src/SumDamage.java
+++ Calculator/src/SumDamage.java
@@ -4,9 +4,9 @@ public class SumDamage {
         for(int i = 0; i < dmgArr.length; ++i){
             health -= dmgArr[i];
             // 현재 피해를 몇 입혔는지 추가로 출력하시오.
-        System.out.println("현재 피해를" + i + "입혔습니다");
+            System.out.println("현재 피해를" + dmgArr[i] + "입혔습니다");
 
-            if(i > health){
+            if(health <= 0){
                 System.out.println("캐릭터가 죽었습니다.");
                 return;
             }
