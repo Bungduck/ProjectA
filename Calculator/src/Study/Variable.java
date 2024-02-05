607d2edb955ae1cc6c6d09cc3717d6dc3d893703
ahn430 <156993886+ahn430@users.noreply.github.com>
2024-02-05 16:00:16 +0900

    study1

--- Calculator/src/Study/Variable.java
+++ Calculator/src/Study/Variable.java
@@ -5,8 +5,9 @@ public class Variable {
         // int 자료형 변수 3개(a, b, c)를 생성해서 처음 두개의 변수(a, b)를 각각 3과 5로 초기화 하고,
         // 나머지 하나의 변수(c)를 처음 두개의 변수(a, b)를 더한 값으로 초기화 하시오.
         // Start
-
-
+        int a = 3;
+        int b = 5;
+        int c = a+b;
         // End
     }
 }
