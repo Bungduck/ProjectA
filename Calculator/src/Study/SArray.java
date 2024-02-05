668444b1097c35a57af687a78eea98763f69e7bc
ahn430 <156993886+ahn430@users.noreply.github.com>
2024-02-05 16:45:41 +0900

    study2

--- Calculator/src/Study/SArray.java
+++ Calculator/src/Study/SArray.java
@@ -1,5 +1,7 @@
 package Study;
 
+import java.lang.reflect.Array;
+
 public class SArray {
     public SArray(){
         // 문자 배열을 10Byte 공간으로 선언한 후,
@@ -7,7 +9,13 @@ public class SArray {
         // System.out.println() 함수를 이용해서
         // 총 0부터 9까지 값이 출력되도록 하시오.
         // Start
+            
+            char[] arr1 = {0,1,2,3,4,5,6,7,8,9};
+            char[] arr2 = new char[i];
 
+            for(char i = 0; i < arr1.length; ++i);
+            System.out.println(arr1[1],arr1[2],arr1[3],arr1[4],arr1[5],arr1[6],arr1[7],arr1[8],arr1[9]);
+        }
         // End
     }
 }
