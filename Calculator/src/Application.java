cc071e40e0b0c2750ea8839c187672856e596e82
U-Jae <77143213+Bungduck@users.noreply.github.com>
2024-02-05 15:11:47 +0900

    Application String Scanner 완성하기
    1

--- Calculator/src/Application.java
+++ Calculator/src/Application.java
@@ -41,20 +41,27 @@ public class Application {
 //        }
 
         // 스캐너
-        Scanner sc = new Scanner(System.in);
-        System.out.print("현재 체력을 입력하시오 : ");
-        int health = sc.nextInt();
-        System.out.print("피해 5번 정도 입력하시오 : ");
-        int[] dmgarr = new int[5];
-        for(int i = 0; i < 5; ++i) dmgarr[i] = sc.nextInt();
-
-        SumDamage SD = new SumDamage();
-        SD.keepHit(health, dmgarr);
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("현재 체력을 입력하시오 : ");
+//        int health = sc.nextInt();
+//        System.out.print("피해 5번 정도 입력하시오 : ");
+//        int[] dmgarr = new int[5];
+//        for(int i = 0; i < 5; ++i) dmgarr[i] = sc.nextInt();
+//
+//        SumDamage SD = new SumDamage();
+//        SD.keepHit(health, dmgarr);
 
 //        SumDamage SD = new SumDamage();
 //        int dmgarr[] = {1, 2, 3, 4, 5};
 //        SD.keepHit(10, dmgarr);
-        
+
+        // Start와 End 사이에서 작업하면 됨.
+        // 문자열(String)을 입력 받아서 반복문을 이용해 문자 하나하나 출력해보기.
+        // Start
+
+
+        // End
+
 
 
     }
