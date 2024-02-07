f296a450192672e5c251b81d0e974d6a7475aefd
ahn430 <156993886+ahn430@users.noreply.github.com>
2024-02-07 16:17:01 +0900

    study03

--- Calculator/src/Study/SIf.java
+++ Calculator/src/Study/SIf.java
@@ -10,6 +10,21 @@ public class SIf {
 
         // Start
 
+        if(score < 60){
+            System.out.println("F");
+        }
+        else if (score < 70){
+            System.out.println("D");
+        }
+        else if (score < 80){
+            System.out.println("C");
+        }
+        else if (score < 90){
+            System.out.println("B");
+        }
+        else if (score < 100){
+            System.out.println("A");
+        }
         // End
     }
 }
