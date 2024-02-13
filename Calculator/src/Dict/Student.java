e59ef478aab626687b14388dc6c65c475ca95f26
ahn430 <156993886+ahn430@users.noreply.github.com>
2024-02-13 14:35:47 +0900

    quiz 6

---
+++ Calculator/src/Dict/Student.java
@@ -0,0 +1,14 @@
+package Dict;
+
+public class Student {
+    public int s1;
+
+    public String name;
+
+    public Student(int code, String name){
+        this.s1 = s1;
+        this.name = name;
+    }
+
+
+}
