7300c846622ca79dc527ceb031574d68b14e8f7e
U-Jae <77143213+Bungduck@users.noreply.github.com>
2024-02-13 15:56:17 +0900

    완성본

--- Calculator/src/Study/Unit.java
+++ Calculator/src/Study/Unit.java
@@ -6,25 +6,35 @@ package Study;
 // 각각의 캐릭터는 체력이 0 이하가 되면 사망하는 메시지를 출력합니다.
 // 각각의 캐릭터는 공격 메소드를 가지고 있습니다.
 public class Unit {
-
     private int hp;
-
     private int atk;
-
     private int def;
 
     public Unit(int hp, int atk, int def) {
         this.hp = hp;
         this.atk = atk;
         this.def = def;
+    }
 
-
-        public void display() {
-            System.out.println(hp + " " + atk + " " + def);
+    public boolean isDead(){
+        if(hp < 0) {
+            System.out.println("캐릭터가 사망했습니다.");
+            return true;
         }
-        
+        else{
+            return false;
+        }
+    }
+
+    public void attack(Unit target){
+        int trueDamage = atk - target.def;
+        if(trueDamage < 0) trueDamage = 0;
+        target.hp -= trueDamage;
     }
 
 
+    public void display() {
+        System.out.println(hp + " " + atk + " " + def);
+    }
 
 }
